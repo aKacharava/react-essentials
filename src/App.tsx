e72d6f11b80cb66233bdeeffc7cd50272f80f23a
Alexander Kacharava <alexander@cube.nl>
2024-08-13 13:33:59 +0200

    Extract component to own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,4 @@
-function Header(
-    {
-        dynamicText
-    }: {
-        dynamicText: string
-    }
-) {
-    const reactDescriptions: string[] = ['Fundemental', 'Core', 'Crucial'];
-
-    function genRandomInt(max: number): number {
-        return Math.floor(Math.random() * (max + 1));
-    }
-
-    const description = reactDescriptions[genRandomInt(2)];
-
-    return (
-        <header className="flex flex-col items-center text-center mx-12 mb-10">
-            <img
-                src="https://images.pexels.com/photos/27054232/pexels-photo-27054232/free-photo-of-licht-fel-luchtig-zee.jpeg"
-                alt="Pexels Photo"
-                className="w-1/3 object-cover"
-            />
-            <h1 className="m-0 text-7xl">
-                React Essentials
-            </h1>
-            <p>
-                { description } React concepts you will need to learn!
-            </p>
-            <p className="m-0 text-lg">
-                { dynamicText } ipsum dolor sit amet, consectetur adipiscing elit. Cras ac quam vehicula ipsum vulputate ultrices.
-                Morbi ullamcorper faucibus lobortis. Sed.
-            </p>
-        </header>
-    );
-}
+import { Header } from "./components/Header.tsx";
 
 function App() {
     return (
