1a16f5cd6a68da735ad5a37216454ad7e6146fe0
Alexander Kacharava <alexander@cube.nl>
2024-08-15 09:05:43 +0200

    Rewrite props param

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,7 @@ import './Header.css';
 import reactConcepts from '../../assets/react-core-concepts.png';
 
 export function Header(
-    {
-        dynamicText
-    }: {
+    props: {
         dynamicText: string
     }
 ) {
@@ -14,7 +12,7 @@ export function Header(
         return Math.floor(Math.random() * (max + 1));
     }
 
-    const description = reactDescriptions[genRandomInt(2)];
+    const description: string = reactDescriptions[genRandomInt(2)];
 
     return (
         <header className="flex flex-col items-center text-center mx-12 mb-10">
@@ -30,7 +28,7 @@ export function Header(
                 { description } React concepts you will need to learn!
             </p>
             <p className="m-0 text-lg">
-                { dynamicText } ipsum dolor sit amet, consectetur adipiscing elit. Cras ac quam vehicula ipsum vulputate ultrices.
+                { props.dynamicText } ipsum dolor sit amet, consectetur adipiscing elit. Cras ac quam vehicula ipsum vulputate ultrices.
                 Morbi ullamcorper faucibus lobortis. Sed.
             </p>
         </header>
